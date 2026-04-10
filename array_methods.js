737330b9c3e4024a698e94d51e31e844e43c541a
ayuushpokharel <ayuushpokharel@gmail.com>
2026-04-10 17:39:28 +0545

    4/10/2026 5:40

--- array_methods.js
+++ array_methods.js
@@ -18,12 +18,12 @@ const callback = (value, index, array) => {
   console.log(value, index, array);
 };
 
-// numbers.forEach(callback);
-// const result = numbers.forEach((v, index) => {
-//   console.log(index, v);
-//   numbers[index] = v * 2;
-// });
-// console.log(numbers);
+numbers.forEach(callback);
+const result = numbers.forEach((v, index) => {
+  console.log(index, v);
+  numbers[index] = v * 2;
+});
+console.log(numbers);
 
 //? map x => y
 const double = numbers.map((value) => {
@@ -45,11 +45,45 @@ console.log(numbers, filterEven);
 
 //! reduce
 let sum = numbers.reduce((acc, value) => {
-    return (acc += value)
+  return (acc += value);
 }, 10); // previous value(acc) => 10
 
-console.log(sum)
+console.log(sum);
 
 //findIndex
+// let index = numbers.findIndex(numbers)
+// console.log(index)
 // every
 // some
+
+//* Example Question
+//? to print the passed students with their average marks
+const student = [
+  {
+    name: "Ram",
+    marks: [40, 50, 33, 76, 80],
+  },
+  {
+    name: "Romish",
+    marks: [43, 45, 36, 26, 70],
+  },
+  {
+    name: "Sita",
+    marks: [20, 80, 29, 66, 78],
+  },
+];
+console.log(student)
+const stu_avg = student.map((student) => {
+  const total_mark = student.marks.reduce((acc, marks) => {
+    return (acc += marks);
+  });
+  const avg = total_mark / student.marks.length;
+  return {
+    name: student.name,
+    marks: student.marks,
+    avg_marks: avg,
+  };
+});
+const filterPass = stu_avg.filter((stu) => stu.avg_marks >= 50)
+const print = filterPass.map((stu) => `${stu.name}-${stu.avg_marks}`)
+console.log(print);
