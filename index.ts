9394ebcd2cb940109e2decb2ac761c23dc7bf9e3
ayuushpokharel <ayuushpokharel@gmail.com>
2026-04-20 17:55:08 +0545

    committed

---
+++ index.ts
@@ -0,0 +1,12 @@
+//! typescript
+//! data types => string, number, boolean, null, undefined
+// any
+// unknown
+//* variable declaration
+// let, const, var
+let str = "hello";
+// str = 23
+
+let num: number;
+num = 12;
+// num = "hello" => error
