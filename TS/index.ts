c4f11b5f30dee5f2a49c5e4dd1ed51e13bcdb936
ayuushpokharel <ayuushpokharel@gmail.com>
2026-04-23 18:23:28 +0545

    4/23/2026 6:23

--- TS/index.ts
+++ TS/index.ts
@@ -128,4 +128,144 @@ users.map((user) => {
   console.log(user.email);
 });
 
+//^  interface => inheritance
+interface ILogin {
+  email: string;
+  password: string;
+}
+interface IReg extends ILogin {
+  userName: string;
+  fullName: string;
+}
+let user_acc: IReg = {
+  fullName: "",
+  userName: "",
+  email: "",
+  password: "",
+};
 
+//! function
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+console.log(add(12, 3));
+// console.log(add("12", "3"));
+
+//& void
+let greet = (name: string): void => {
+  console.log(name);
+};
+
+//& never
+const never = (): never => {
+  throw "new error";
+};
+
+const add1 = (...numbers: number[]) => {
+  // return a + b:
+};
+
+//* union [ | ]
+let id: number | string = 12;
+id = 2;
+id = "ayu";
+
+//* type [type alias]
+type A = {
+  name?: string;
+  email: string;
+  password: string;
+};
+// let b1:A ={
+
+type addF = (x: number, y: number) => number;
+const add2: addF = (a, b) => {
+  return a + b;
+};
+
+//* intersection [ & ]
+type B = {
+  name: string;
+};
+type C = A & B;
+type D = A | B;
+let c1: C = {
+  name: "romish",
+  email: "ro@gmail.com",
+  password: "2324344",
+};
+let d1: D = {
+  name: "john",
+};
+
+//& diff between type and inheritance
+// interface => only for object , type => can declare for object, functions...
+// interface combine => using keyword extend , type => union and intersection
+// interface => automatically merged if declared multiple times, type => can't be declared multiple times
+interface E {
+  e: string;
+}
+
+interface E {
+  f: string;
+}
+
+let e: E = {
+  e: "",
+  f: "",
+};
+// type E1 = {
+//   e: string;
+// }
+
+// type E1 = {
+//   f: string;
+// }
+
+// let e: E = {
+//   e: "",
+//   f: "",
+// };
+
+//* type literal
+let res_status: "success" | "fail" | "error";
+type status = "success" | "fail" | "error";
+let Status: status;
+res_status = "success";
+// res_status = "hello" => error
+
+//* class
+class Person {
+  name: string;
+  email: string;
+  private password: string;
+  public age: number;
+  constructor(name: string, email: string, password: string, age: number) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.age = age;
+  }
+}
+let obj1 = new Person("romish", "ro@gmail.com", "12345", 12);
+
+//*  generic type
+
+interface IBox<T> {
+  value: T;
+}
+let str_box: IBox<string> = {
+  value: "",
+};
+let num_box: IBox<number> = {
+  value: 12,
+};
+
+type ideF = <T>(a: T) => T;
+const identity: ideF = (a) => {
+  return a;
+};
+console.log(identity(12));
+console.log(identity("abc"));
+
+//! utility type => hw
