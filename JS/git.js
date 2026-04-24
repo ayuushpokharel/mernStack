23937a7cc52ba6bd5fbaed7ab1e506e2f2a81368
ayuushpokharel <ayuushpokharel@gmail.com>
2026-04-24 17:53:45 +0545

    git

---
+++ JS/git.js
@@ -0,0 +1,16 @@
+//! git & github/gitlab/bitbucket
+//& git => vcs
+
+//* commands
+//&  git config --global --list => list local git global config
+//& git config --global user.email "abc@gmail.com"
+
+//& git init => initialize empty git repo at folder
+
+//* WD (working directory) => Staging Area 
+
+//! git add <file_path> => wd  -> SA
+//! git commit -m "<commit message>" => SA -> local branch/repo
+//! git status
+//! git add . => all changes -> SA
+//! git push 
