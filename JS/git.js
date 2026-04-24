023800e001718b2748fbc5fb5d9f2bf069c5d287
ayuushpokharel <ayuushpokharel@gmail.com>
2026-04-24 18:12:08 +0545

    merge

--- JS/git.js
+++ JS/git.js
@@ -7,16 +7,17 @@
 
 //! git init => initialize empty git repo at folder
 
-//* WD (working directory) => Staging Area 
+//* WD (working directory) => Staging Area
 
 //! git add <file_path> => wd  -> SA
 //! git commit -m "<commit message>" => SA -> local branch/repo
 //! git status
 //! git add . => all changes -> SA
-//! git push 
+//! git push
 
 //* branches
 //! git branch => list all local branches
 //! git branch <branch_name> => create new branch
 //! git switch <branch_name> => switches to the branch_name
 
+//! git merge <branch_name>
